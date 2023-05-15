479443bf5d2539df5ac1c32f42df1e88d23499a9
masrur arefin sadhin <masrur.arefin@bracits.com>
2023-05-15 19:13:38 +0600

    job list

---
+++ src/app/candidate-user/create-resume/create-resume.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-resume',
+  templateUrl: './create-resume.component.html',
+  styleUrls: ['./create-resume.component.css']
+})
+export class CreateResumeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
